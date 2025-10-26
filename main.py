b954e489000390936ff1191e24bd5b5c78b21cb8
Arpan_Paul <arpanpaul440@gmail.com>
2025-10-27 02:58:19 +0530

    v1

--- main.py
+++ main.py
@@ -1,75 +1,89 @@
-'''Q. Use os module to-->
-Rename files inside a folder as "1" to "n","n" is "total no. of files inside".
-take "file format" as argument'''
-
+#clear the clutter
 import os
 
 
-class Rename:
-    def __init__(self,Dpath:str,Ftype:str):
-        """Dpath-> Directory Path , Ftype-> File Type Extension"""
-
-        self.Dpath=Dpath
-        self.Ftype=Ftype.lower()
-        self.Check_Set_Path(Dpath) #checking if path is accesible.
-    
-    def Check_Set_Path(self,path):
-        """Checks directory is accessible or not.
-        \nTrue->set path 
-        \nFalse->print 'Directory Inaccessible'"""
-
-        if os.access(path,os.F_OK)==False:       
-            print("Directory Inaccessible")
-            exit()
-        else: 
-            os.chdir(path)                #change directory to Dpath
-            if os.getcwd()==path:
-                print(f"Directory Updated to {path}")
-            else:
-                print(f"Directory not updated.\n Current directory: {os.getcwd()}")
-
-    def Files(self):
-        with os.scandir(self.Dpath) as DirEntires:
-
-            self.File_no=0                      #initialising file name counter
-            for self.entry in DirEntires:       #iterating files in scandir iterable
-
-                if (not self.entry.name.startswith(".")) and self.entry.is_file() and self.entry.name.endswith(f".{self.Ftype}"):      
-                    #checking whether entry is hidden, file, and File type
-                    
-                    self.File_no+=1    #increasing file name counter       
-                    newname=f"{str(self.File_no)}.{self.Ftype}"
-                    
-                    temp=self.renamed(newname)
-                    print(temp)
-                    
-                    
-                    
-            if self.File_no!=0:
-                print (f"All {self.Ftype} files in {self.Dpath} renamed.")
-            else:
-                print(f"No file of type {self.Ftype} found in \n{self.Dpath}")
-
-    def renamed(self,newname: str)->str:
-        """Checks if file name already exists.
-        \n if true then produces new file name"""
-        if not os.path.exists(newname):
-            os.rename(self.entry.name,newname)              
-            return f"{self.entry.name} --> {newname}"
-        else:
-            self.File_no+=1
-            newname=f"{str(self.File_no)}.{self.Ftype}"
-            return self.renamed(newname) 
+def Check_Set_path(Dpath)->None:
+    """Dpath-> Directory to change
+     \nChecks directory is accessible or not:
+        \n  True->chdir Dpath & return True \n  raise DirectoryNotFoundError
         
-        """if you dont return then when ever name conflict arises, name will get changed but fucntion will return NONE!"""
+        \n  if True but not updated to Dpath:
+            return False"""
+    
+    if os.access(Dpath,os.F_OK)==False:       
+        # print("Directory Inaccessible")
+        raise FileNotFoundError("DirectoryNotFoundError")
+    else:
+        old=os.getcwd()
+        os.chdir(Dpath)   #change directory to Dpath
+        if os.getcwd()!=old:
+            print(f"Directory Updated to {Dpath}")
+            return True
+        else:
+            print(f"Directory not updated.\n Current directory: {os.getcwd()}")
+            return False
 
-if __name__=="__main__":
-    print("-----CLUTTER CLEARER INITIALISED-----")
+def files(Ftype:str,Dpath:str)->None:
+    """Ftype-> File Type to change 
+    \nDpath-> Directory to change"""
+    with os.scandir() as DirEntires:
+        files.File_no=0               #file name counter
+        for file in DirEntires:    
 
-    Dir=input("Enter directory path:")
-    ext=input("Enter File Extension of files to rename:")
+            if file.is_file():
+                if (not file.name.startswith(".")) and file.name.endswith(f".{Ftype}"):
+                    #hidden file? and reqired type?
 
-    test=Rename(Dir,ext)
-    test.Files()
+                    files.File_no+=1     
+                    newname=f"{str(files.File_no)}.{Ftype}"
+                    
+                    frenamed=renamed(file,newname,Ftype)
+                    print(frenamed) #printing renamed confirmation
+        if files.File_no!=0:
+            print (f"All {Ftype} files in {Dpath} renamed.")
+        else:
+            print(f"No file of type {Ftype} found in \n{Dpath}")
+
+def renamed(file,newname: str,Ftype: str)->str:
+    """file-> DirEntry object
+    \nnewname->New generated name using counter
+    \nFtype-> File Type to change
+    
+    \nChecking does name exist:
+    \n  if True: generate new name
+    \n  if False: rename and return confirmation string"""
+    if not os.path.exists(newname):
+        os.rename(file.name,newname)   #since file name does not exist renaming           
+        return f"{file.name} --> {newname}" #return confirmation
+    else:
+        files.File_no+=1
+        newname=f"{str(files.File_no)}.{Ftype}"
+        return renamed(file,newname,Ftype) 
+        
+    """if you dont return then when ever name conflict arises, name will get changed but function will return NONE!"""
+   
+def run():
+    """Dpath-> Directory Path , Ftype-> File Type Extension"""
+    try:
+        print("-----CLUTTER CLEARER INITIALISED-----")
+        print("Current Path:",os.getcwd())
+        Dpath=input("Enter directory path:")
+        if Check_Set_path(Dpath):
+            Ftype=input("Enter File Extension of files to rename:")
+            files(Ftype,Dpath)
+        else:
+            if not os.access(Dpath,os.R_OK):
+                print("{Dpath} accessible but not readable")
+            else:
+                if not os.access(Dpath,os.W_OK):
+                    print("{Dpath} accessible and readable but not writable")
+
+    except FileNotFoundError:
+        print(f"Directory:\n{Dpath}\n not found.\nPlease restart the application and try again")
+    finally:
+        pass
+        #add code to restrat program
 
 
+#test
+run()
